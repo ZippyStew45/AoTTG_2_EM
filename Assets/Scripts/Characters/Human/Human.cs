bbcd6c289eafe32c5672e5f0b6da808fad871c76
Senssen <eneslovelycorner@gmail.com>
2024-01-25 19:24:43 +0100

    Removed partial classes in favour of regions.

--- Assets/Scripts/Characters/Human/Human.cs
+++ Assets/Scripts/Characters/Human/Human.cs
@@ -22,7 +22,7 @@ using Weather;
 namespace Characters
 {
 
-    internal partial class Human : BaseCharacter
+    internal class Human : BaseCharacter
     {
         // setup
         public HumanComponentCache HumanCache;
@@ -107,6 +107,10 @@ namespace Characters
         private float _hookHumanConstantTimeLeft;
         private bool _isReelingOut;
         private Dictionary<BaseTitan, float> _lastNapeHitTimes = new Dictionary<BaseTitan, float>();
+
+        //Expedition Extension
+        public string targetTag = "SupplySpawnable";
+        public float targetRadius = 10f;
 
 
         [PunRPC]
@@ -2543,6 +2547,59 @@ namespace Characters
                 _state = value;
             }
         }
+        #region EXPEDITION EXTENSION HUMAN
+
+        //THUNDERSPEAR EDIT
+        public void DieToTS() //Added by Momo Dec 6 2023 to kill people too close to the explosion. and print 100 damage.
+        {
+            GetHit("Thunderspear", 100, "Thunderspear", "");
+            Die();
+        }
+        //SUPPLY SPECIAL EDIT //Added by Momo Dec 5 2023 to use a seperate refill for supply special.
+        public bool RefillSS()
+        {
+            State = HumanState.RefillSS;
+            ToggleSparks(false);
+            CrossFade(HumanAnimations.Refill, 0.1f);
+            PlaySound(HumanSounds.Refill);
+            _stateTimeLeft = Cache.Animation[HumanAnimations.Refill].length;
+            return true;
+        }
+        public void FinishRefillSS() //Added by Momo Dec 5 2023 to use a seperate refill for supply special.
+        {
+            CurrentGas += 15f;
+            if (CurrentGas > MaxGas)
+            {
+                CurrentGas = MaxGas;
+            }
+        }
+
+        //PHYSICS EDIT
+        public void FixedUpdateStandStill(Vector3 gravity) //Added by Sysyfus Jan 19 2024 so characters can stand still and not slide down slopes they can stand on
+        {
+            if (CanJump() && Cache.Animation.IsPlaying(StandAnimation) && Cache.Rigidbody.velocity.magnitude < 1f)
+            {
+                Vector3 inverseGravity = -gravity * Mathf.Clamp(((Cache.Rigidbody.velocity.magnitude - 0.8f) / -0.55f), 0f, 1f);
+                Cache.Rigidbody.AddForce(inverseGravity);
+            }
+        }
+
+        //SURVEY POINT DETECTION
+        private void OnTriggerEnter(Collider other)
+        {
+            //Added by Momo to check for Survey Point collision on 18th january 2024
+            if (other.gameObject.tag == "SurveyPoint")
+            {
+                BoxCollider collider = other.gameObject.GetComponent<BoxCollider>();
+                MeshRenderer obj = other.gameObject.GetComponentInChildren<MeshRenderer>();
+                AudioSource audio = other.gameObject.GetComponentInChildren<AudioSource>();
+                ParticleSystem ps = other.gameObject.GetComponentInChildren<ParticleSystem>();
+                collider.enabled = false;
+                RPCManager.PhotonView.RPC("CollectEmblemRPC", RpcTarget.AllViaServer, collider, obj, audio, ps);
+            }
+        }
+
+        #endregion
     }
 
     public enum HumanState
@@ -2589,4 +2646,5 @@ namespace Characters
         Left,
         Right
     }
+
 }
